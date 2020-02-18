f7ce3387bdb4baab8191bef8ba72f98e9e0d9954
rupesh123456 <rupeshkumarmishra32@gmail.com>
2020-02-18 21:57:10 +0530

    Mind Install

---
+++ screens/Onboarding.js
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Dimensions
+} from "react-native";
+import { Block, Button, Text, theme } from "galio-framework";
+
+const { height, width } = Dimensions.get("screen");
+
+import argonTheme from "../constants/Theme";
+import Images from "../constants/Images";
+
+class Onboarding extends React.Component {
+  render(){
+  
+    const { navigation } = this.props;
+
+   return (
+      <Block flex style={styles.container}>
+        <StatusBar hidden />
+        <Block flex center>
+        <ImageBackground
+            source={Images.Onboarding}
+            style={{ height, width, zIndex: 1 }}
+          />
+        </Block>
+        <Block center>
+          <Image source={Images.LogoOnboarding} style={styles.logo} />
+        </Block>
+        <Block flex space="between" style={styles.padded}>
+            <Block flex space="around" style={{ zIndex: 2 }}>
+              <Block style={styles.title}>
+                <Block>
+                  <Text color="white" size={50}>
+                    SOFTWARE
+                  </Text>
+                </Block>
+                <Block>
+                  <Text color="white" size={50}>
+                    STARTUP
+                  </Text>
+                </Block>
+                
+              </Block>
+              <Block center>
+                <Button
+                  style={styles.button}
+                  color={argonTheme.COLORS.SECONDARY}
+                  onPress={() => navigation.navigate("Home")}
+                  textStyle={{ color: argonTheme.COLORS.BLACK }}
+                >
+                  Get Started
+                </Button>
+              </Block>
+          </Block>
+        </Block>
+      </Block>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.COLORS.BLACK
+  },
+  padded: {
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    position: "relative",
+    bottom: theme.SIZES.BASE,
+    zIndex: 2,
+  },
+  button: {
+    width: width - theme.SIZES.BASE * 4,
+    height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0
+  },
+  logo: {
+    width: 320,
+    height: 60,
+    zIndex: 2,
+    position: 'relative',
+    marginTop: '-50%'
+  },
+  title: {
+    marginTop:'-5%'
+  },                                            
+  subTitle: {
+    marginBottom: 300
+    
+  },
+subTitle1:{
+    
+    marginTop: 20
+    
+  },
+
+});
+
+export default Onboarding;
